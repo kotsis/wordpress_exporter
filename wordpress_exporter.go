bfb780d0e51b5b5326cfd234d5908c82a1691f92
Konstantinos Katsamakas <kotsis@users.noreply.github.com>
2018-07-14 17:00:58 +0300

    Added cli parameter -wpconfig=path support and load wp-config.php into memory

--- wordpress_exporter.go
+++ wordpress_exporter.go
@@ -5,9 +5,47 @@ import (
 
   log "github.com/Sirupsen/logrus"
   "github.com/prometheus/client_golang/prometheus/promhttp"
+
+  "flag"
+  "fmt"
+  "os"
+  "io/ioutil"
+  "strings"
 )
 
 func main() {
+
+  wpConfPtr := flag.String("wpconfig", "", "Path for wp-config.php file of the WordPress site you wish to monitor")
+
+  flag.Parse()
+
+  if *wpConfPtr == "" {
+    //no path supplied error
+    fmt.Fprintf(os.Stderr, "flag -wpconfig=/path/to/wp-config/ required!\n")
+    os.Exit(1)
+  } else{
+    var wpconfig_file strings.Builder
+    wpconfig_file.WriteString(*wpConfPtr)
+
+    if strings.HasSuffix(*wpConfPtr, "/") {
+        wpconfig_file.WriteString("wp-config.php")
+    }else{
+        wpconfig_file.WriteString("/wp-config.php")
+    }
+
+    //try to read wp-config.php file from path
+    dat, err := ioutil.ReadFile(wpconfig_file.String())
+    //check(err)
+    if(err != nil){
+        panic(err)
+    }
+    //fmt.Print(string(dat))
+
+    //We must locate with regular expressions the MySQL connection credentials and the table prefix
+    //to-do
+    
+  }
+
   //This section will start the HTTP server and expose
   //any metrics on the /metrics endpoint.
   http.Handle("/metrics", promhttp.Handler())

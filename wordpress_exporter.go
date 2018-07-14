750d1454c809c2b41f519717f0e54308db817285
Konstantinos Katsamakas <kotsis@users.noreply.github.com>
2018-07-14 17:58:07 +0300

    Adds support in parsing wp-config.php and extracting connection credentials

--- wordpress_exporter.go
+++ wordpress_exporter.go
@@ -11,6 +11,7 @@ import (
   "os"
   "io/ioutil"
   "strings"
+  "regexp"
 )
 
 func main() {
@@ -40,10 +41,53 @@ func main() {
         panic(err)
     }
     //fmt.Print(string(dat))
+    fmt.Printf("Read :%v bytes\n", len(dat))
 
     //We must locate with regular expressions the MySQL connection credentials and the table prefix
-    //to-do
+    //define('DB_HOST', 'xxxxxxx');
+    r, _ := regexp.Compile(`define\(['"]DB_HOST['"].*?,.*?['"](.*?)['"].*?\);`)
+    res := r.FindStringSubmatch(string(dat[:len(dat)]))
+    if(res == nil){
+        fmt.Fprintf(os.Stderr, "Error could not find DB_HOST in wp-config.php ...\n")
+        os.Exit(1)
+    }
+    db_host := res[1]
     
+    //define('DB_NAME', 'xxxxxxx');
+    r, _ = regexp.Compile(`define\(['"]DB_NAME['"].*?,.*?['"](.*?)['"].*?\);`)
+    res = r.FindStringSubmatch(string(dat[:len(dat)]))
+    if(res == nil){
+        fmt.Fprintf(os.Stderr, "Error could not find DB_NAME in wp-config.php ...\n")
+        os.Exit(1)
+    }
+    db_name := res[1]
+
+    //define('DB_USER', 'xxxxxxx');
+    r, _ = regexp.Compile(`define\(['"]DB_USER['"].*?,.*?['"](.*?)['"].*?\);`)
+    res = r.FindStringSubmatch(string(dat[:len(dat)]))
+    if(res == nil){
+        fmt.Fprintf(os.Stderr, "Error could not find DB_USER in wp-config.php ...\n")
+        os.Exit(1)
+    }
+    db_user := res[1]
+
+    //define('DB_PASSWORD', 'xxxxxxx');
+    r, _ = regexp.Compile(`define\(['"]DB_PASSWORD['"].*?,.*?['"](.*?)['"].*?\);`)
+    res = r.FindStringSubmatch(string(dat[:len(dat)]))
+    if(res == nil){
+        fmt.Fprintf(os.Stderr, "Error could not find DB_PASSWORD in wp-config.php ...\n")
+        os.Exit(1)
+    }
+    db_password := res[1]
+
+    //$table_prefix  = 'wp_';
+    r, _ = regexp.Compile(`$table_prefix.*?=.*?['"](.*?)['"];`)
+    res = r.FindStringSubmatch(string(dat[:len(dat)]))
+    if(res == nil){
+        fmt.Fprintf(os.Stderr, "Error could not find $table_prefix in wp-config.php ...\n")
+        os.Exit(1)
+    }
+    table_prefix := res[1]
   }
 
   //This section will start the HTTP server and expose

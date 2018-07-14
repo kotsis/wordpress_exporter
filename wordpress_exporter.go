cde9a24dad684bf1d4ab4ce20351182d714ecb4d
Konstantinos Katsamakas <kotsis@users.noreply.github.com>
2018-07-14 16:24:30 +0300

    Initial commit with dependencies

---
+++ wordpress_exporter.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "net/http"
+
+  log "github.com/Sirupsen/logrus"
+  "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+  //This section will start the HTTP server and expose
+  //any metrics on the /metrics endpoint.
+  http.Handle("/metrics", promhttp.Handler())
+  log.Info("Beginning to serve on port :8888")
+  log.Fatal(http.ListenAndServe(":8888", nil))
+}
+
